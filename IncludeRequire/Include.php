e65686a900a4d26bf97bdea2413b252ad549c2da
Rishab Raj <rr0948555@gmail.com>
2026-04-28 11:43:41 +0530

    Include

---
+++ IncludeRequire/Include.php
@@ -0,0 +1,17 @@
+<?php function sayhello() 
+    { 
+        echo "Hello all";
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Use of Inlcude</title>
+</head>
+<body>
+  <h1> Welcome we are Learning Include in </h1>
+   <h3> <?php sayhello(); ?> </h3>
+</body>
+</html>
