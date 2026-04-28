4d64cc4d24f5af0150e08d12980e8f92071677e9
Rishab Raj <rr0948555@gmail.com>
2026-04-28 13:03:57 +0530

    Include Once and Require Once

---
+++ IncludeOnceRequireOnce/index.php
@@ -0,0 +1,8 @@
+<?php include("includes/header.php"); ?>
+
+<h3> This is from index.php </h3>
+
+<!-- include_once will include the file one time only -->
+
+<?php include_once("includes/header.php"); ?>
+<?php include("includes/footer.php"); ?>
