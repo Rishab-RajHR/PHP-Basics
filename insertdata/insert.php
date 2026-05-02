c078e8e53d2ae7a10e36f7d079c3d40688f07329
Rishab Raj <rr0948555@gmail.com>
2026-05-02 11:52:32 +0530

    Insert Data

--- insertdata/insert.php
+++ insertdata/insert.php
@@ -3,5 +3,14 @@
   $name =  $_POST['name'];
   $email =  $_POST['email'];
 
-  mysqli_connect('localhost','root','','insertdb');
+  $con = mysqli_connect('localhost','root','','insertdb');
+
+   $query = "INSERT INTO `userdata`( `Name`, `Email`) VALUES ('$name','$email')";
+
+   $run = mysqli_query($con,$query);
+
+   if($run == TRUE)
+       echo "Data Inserted Successfully <a href='index.php'>Click here to insert more.</a>";
+   else 
+      echo "Error !";
 ?>
