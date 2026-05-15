17281ad42ddd22ca85d9112a86aae6f3f854a0f6
Rishab Raj <rr0948555@gmail.com>
2026-05-15 19:19:29 +0530

    This

--- Oops/this.php
+++ Oops/this.php
@@ -12,16 +12,24 @@ class Student
 {
      private $stu_name;
 
-     function getData($name)
+     function getData($sname)
      {
-        
+         $this->stu_name=$sname;
+     }
+     function showData()
+     {
+        echo $this->stu_name;
      }
 }
 
 $purvik = new Student();
 $purvik->getData("Purvik");
+$purvik->showData();
+
 $ramesh = new Student();
 $ramesh->getData("Ramesh");
+$ramesh->showData();
+
 ?>
 
 </body>
