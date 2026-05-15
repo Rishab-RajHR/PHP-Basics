791d67c2677cf588d9121ca0f3829bcb9520b3a4
Rishab Raj <rr0948555@gmail.com>
2026-05-15 19:10:55 +0530

    this

---
+++ Oops/this.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>This Keyword</title>
+</head>
+<body>
+<?php
+
+class Student
+{
+     private $stu_name;
+
+     function getData($name)
+     {
+        
+     }
+}
+
+$purvik = new Student();
+$purvik->getData("Purvik");
+$ramesh = new Student();
+$ramesh->getData("Ramesh");
+?>
+
+</body>
+</html>
