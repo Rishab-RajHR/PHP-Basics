5b31ec349cfb68e50eae3add708fee219ca14d41
Rishab Raj <rr0948555@gmail.com>
2026-05-16 10:49:16 +0530

    Constructor

---
+++ Oops/constructor.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Constructor in PHP</title>
+</head>
+<body>
+<?php
+   
+class Example{
+    
+    public function sayHello(){
+         
+       echo "Hello World";
+    }
+}
+$obj = new Example();
+
+$obj->sayHello();
+?>
+</body>
+</html>
