c3782a5d217feab23c92a0b7c6c6b187f9995da0
Rishab Raj <rr0948555@gmail.com>
2026-05-16 13:06:35 +0530

    Address

---
+++ address/ipadd.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>IP Address Detection</title>
+</head>
+<body>
+<?php 
+  $ip = $_SERVER['REMOTE_ADDR'];
+?>
+</body>
+</html>
